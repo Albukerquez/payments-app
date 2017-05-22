5dd18448dbe9a81e1b8674475ac8031ff1769d10
Eugene Schwartz <nonsenseftw@yandex.ru>
2017-05-23 01:38:13 +0500

    added stripe

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,9 +1,15 @@
 class PublicationsController < ApplicationController
+  before_action :check_for_subscription, only: :show
   def index
     @publications = Publication.all
   end
 
   def show
     @publication = Publication.find(params[:id])
+  end
+
+  def check_for_subscription
+    redirect_to publications_path unless current_user.subscription.active
+    flash[:alert] = 'Вы должны оформить подписку, чтобы посмотреть содержимое'
   end
 end
